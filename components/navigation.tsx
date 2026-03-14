9b1242c7a0d52b94889422499016d6221958e727
nishan.paul.hello@gmail.com <nishan.paul.hello@gmail.com>
2026-03-14 14:58:16 +0600

    feat: Increase navigation icon size and update its hover animation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -41,9 +41,9 @@ export function Navigation() {
             <Image
               src="/icon.svg"
               alt="KAI"
-              width={28}
-              height={28}
-              className="h-7 w-7 transition-transform group-hover:rotate-12"
+              width={36}
+              height={36}
+              className="h-9 w-9 transition-transform duration-300 group-hover:scale-110 translate-y-[1px]"
             />
           </div>
           <span className="text-lg font-bold font-mono tracking-tighter">KAI</span>
